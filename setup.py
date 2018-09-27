4914fbde12a123af2b240358bd3842f581ebdbeb
d-babiak <d.babiak@gmail.com>
2018-09-26 20:35:50 -0700

    Bump 0.0.3 -> 0.0.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ URL = 'https://github.com/leantaas/postgoose'
 EMAIL = 'd.babiak@gmail.com'
 AUTHOR = 'dmb'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 
 REQUIRED = ['psycopg2>=2.7.5'] # todo: 'psycopg2==2.7.5 --no-binary psycopg2'
 
